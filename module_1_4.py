12b57d038161c9a6d6d27b2fecc2e02bc9d27a66
Viktrolla <viktoria.kozlovskaya22@gmail.com>
2024-11-29 21:04:33 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,4 @@
+Course = "Python"
+total_tasks = 12
+hours_spent = 1.5
+print ('Курс:',Course,'всего задач:',total_tasks,'затрачено часов:',hours_spent,'среднее время выполнения задач:',hours_spent/total_tasks)
