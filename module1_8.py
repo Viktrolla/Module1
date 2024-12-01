330cfa715ec5747bfadeb81f8c49654d31ac3c4d
Viktrolla <viktoria.kozlovskaya22@gmail.com>
2024-12-01 17:43:51 +0300

    Add files via upload

---
+++ module1_8.py
@@ -0,0 +1,15 @@
+my_dict = {'Eva': 1997, 'Vika': 1996, 'Alex': 1995}
+print(my_dict)
+print(my_dict['Eva'])
+print(my_dict.get('Fillip'))
+my_dict.update({'Fillip':1994, 'Helen':1989})
+print (my_dict)
+del my_dict['Eva']
+print(my_dict)
+my_set = {1,1,3,3,'Apple','Horse','Apple','Horse'}
+print(my_set)
+print(my_set.add('peach'))
+print(my_set.add(5))
+print(my_set)
+print(my_set.discard(1))
+print(my_set)
