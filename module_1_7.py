a569c7308e5a9998515cb7f00c567eb01d7acca4
Viktrolla <viktoria.kozlovskaya22@gmail.com>
2024-12-01 16:37:02 +0300

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,7 @@
+immutable_var = 1,2,True,'Hello'
+print(immutable_var)
+#immutable_var[0] = 200 # кортеж не поддерживает обращение по элементам
+mutable_list = ['horse','dog','peach']
+print(mutable_list)
+mutable_list[2] = 'bird'
+print (mutable_list)
